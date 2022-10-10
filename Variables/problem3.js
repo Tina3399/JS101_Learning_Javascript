cf768e4891f8b942cec8566c7c5a88a4e26b308c
Tina Rawat <tinarawat03@gmail.com>
2022-10-10 22:48:13 +0530

    modified

--- Variables/problem3.js
+++ Variables/problem3.js
@@ -1,7 +1,10 @@
 let myName = "Tina Rawat";
 let age = 21;
+let school = "Masai School";
 
 console.log(myName);
 console.log(typeof myName);
 console.log(age);
 console.log(typeof age);
+console.log(school);
+console.log(typeof school);
