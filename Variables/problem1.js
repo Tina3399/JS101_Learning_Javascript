51d0167a1a19d15a3bed1b9a0416eecccc33966c
Tina Rawat <tinarawat03@gmail.com>
2022-10-10 22:42:47 +0530

    modified prob1

--- Variables/problem1.js
+++ Variables/problem1.js
@@ -2,3 +2,5 @@ console.log("Masai School");
 console.log("A transfermation in education worldwide");
 console.log("Hello World");
 console.log("Tina Rawat");
+console.log("Neeraj Rawat");
+console.log("Ruchi Rawat");
